954327bccc59a5009d790e421e3742734e4df3a9
Deihl_Reyes <78899978+DeihlReyes@users.noreply.github.com>
2024-06-02 21:54:07 +0800

    syle: fix carousel

---
+++ components/project-carousel.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import Image from "next/image";
+import React, { useEffect, useState } from 'react';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { CarouselApi } from "@/components/ui/carousel";
+
+const ProjectCarousel = ({ projectImages }: { projectImages: any[] }) => {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  return (
+    <div>
+      <Carousel setApi={setApi} opts={{ loop: true }} className="w-[320px] md:w-[650px] h-full md:h-[380px]">
+        <CarouselContent>
+          {projectImages.map((img: any, index: any) => (
+            <CarouselItem className="w-full md:w-[650px] h-full md:h-[380px]" key={index}>
+              <Image
+                className="w-[650px] h-full md:h-[380px] object-cover rounded-xl shadow-xl"
+                src={img}
+                alt={`Project Image ${index + 1}`}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+      <div className="flex flex-row gap-2">
+        {Array.from({ length: count }, (_, i) => (
+          <span
+            key={i}
+            className={`w-2 h-2 rounded-full mx-1 ${i === current - 1 ? 'bg-black' : 'bg-gray-300'}`}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCarousel;
