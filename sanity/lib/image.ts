53c77bf71efdf07982a2bd88ef6f16ebbe0d30be
DeihlReyes <deihlreyes7@gmail.com>
2026-03-23 22:27:53 +0800

    fix sanity image issue

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,5 +1,5 @@
 import { createImageUrlBuilder as imageUrlBuilder } from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import type { SanityImageSource } from "@sanity/image-url";
 import { client } from "./client";
 
 const builder = imageUrlBuilder(client);
